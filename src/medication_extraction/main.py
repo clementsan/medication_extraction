824b62338ebbc6e0edcfa13f97ae54d2da6e50de
Clement Vachet <1039042+clementsan@users.noreply.github.com>
2025-06-27 12:28:27 -0600

    Add type annotations for cli arguments and options

--- src/medication_extraction/main.py
+++ src/medication_extraction/main.py
@@ -15,30 +15,26 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    input_pdf: str = typer.Option(..., "--input-pdf", help="Input PDF file"),
-    output_dir: str = typer.Option(..., "--output-dir", help="Output folder"),
+    input_pdf: Annotated[str, typer.Option(help="Input PDF file")],
+    output_dir: Annotated[str, typer.Option(help="Output folder")],
+    ocr_model: Annotated[str, typer.Option(help="OCR model")] = "mistral-ocr-latest",
+    text_model: Annotated[str, typer.Option(help="LLM model")] = "ministral-8b-latest",
     qc_ocr: Annotated[
         bool, typer.Option(help="Quality Control - save OCR output file")
     ] = False,
     direct_qna: Annotated[
         bool, typer.Option(help="Direct Question & Answer - OCR + LLM combined")
     ] = False,
-    ocr_model: str = typer.Option(
-        "mistral-ocr-latest", "--ocr-model", help="OCR model"
-    ),
-    text_model: str = typer.Option(
-        "ministral-8b-latest", "--text-model", help="LLM model"
-    ),
 ):
     """Main command"""
     logging.basicConfig(level=logging.INFO)
     data_extractor = pipeline.MedicalDataExtractor(
         input_pdf=input_pdf,
         output_dir=output_dir,
-        qc_ocr=qc_ocr,
-        direct_qna=direct_qna,
         ocr_model=ocr_model,
         text_model=text_model,
+        qc_ocr=qc_ocr,
+        direct_qna=direct_qna,
     )
     data_extractor.run_workflow()
 
