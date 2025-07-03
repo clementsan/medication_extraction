e74989e1f25338f812e7f9e309fe1d4ce6fa45c0
Clement Vachet <1039042+clementsan@users.noreply.github.com>
2025-07-03 14:20:56 -0600

    Enable document retrieval strategy

--- src/medication_extraction/extraction.py
+++ src/medication_extraction/extraction.py
@@ -7,15 +7,19 @@ import json
 import logging
 from typing import Any, Dict
 
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_mistralai import MistralAIEmbeddings
+
 from medication_extraction import schema
 from medication_extraction import utils
 
 logger = logging.getLogger(__name__)
 
 
-def generate_llm_prompt(prompt_type: str, context: str = "") -> str:
+def retrieve_llm_prompt(prompt_type: str) -> str:
     """
-    Generate LLM prompt, leveraging prompt template file
+    Retrieve LLM prompt, leveraging prompt template file
     Args:
         - prompt_type: select specific prompt from JSON file
         - context: optional context
@@ -33,19 +37,73 @@ def generate_llm_prompt(prompt_type: str, context: str = "") -> str:
     prompt_template = prompt_dict[prompt_type]
     logger.debug("Prompt template: \n%s", prompt_template)
 
-    # Optional - Add context to prompt
-    if context:
-        prompt_template = prompt_template.format(context=context)
-
     return prompt_template
 
 
-def llm_extraction(
-    text_model: str, mistral_client: object, pdf_content: str
-) -> Dict[str, Any]:
-    """Data extraction via LLM"""
+def doc_retrieval(pdf_content: str, query) -> str:
+    """
+    Perform doc retrieval using mistral embedding and chromadb vector database
+    Args:
+        - pdf_content: input document
+        - query: user query for document retrieval
+    Returns:
+        - retrieved content
+    """
 
-    prompt = generate_llm_prompt(prompt_type="prompt_context", context=pdf_content)
+    # Step 1 - Split PDF content based on markdown content
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        # ("##", "Header 2"),
+    ]
+    markdown_splitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on, strip_headers=False
+    )
+    md_header_splits = markdown_splitter.split_text(pdf_content)
+    logger.debug("\nNumber document splits: %d", len(md_header_splits))
+
+    # Step 2 - Generate embeddings for each text chunk (via Mistral Embeddings API)
+    text_embeddings = MistralAIEmbeddings(model="mistral-embed")
+
+    # Step 3- Add embeddings to in-memory vector store
+    vector_store = InMemoryVectorStore(text_embeddings)
+    # Index chunks
+    _ = vector_store.add_documents(documents=md_header_splits)
+
+    # Step 4 - Perform document retrieval via similarity search on query
+    retrieved_docs = vector_store.similarity_search(query, k=4)
+    retrieved_results = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+    return retrieved_results
+
+
+def llm_extraction(
+    text_model: str, mistral_client: object, pdf_content: str, rag: bool = False
+) -> Dict[str, Any]:
+    """
+    Data extraction via LLM
+    Args:
+        - text_model: LLM text model
+        - mistral_client: mistral client
+        - pdf_content: input document
+        - rag: optional retrieval strategy
+    Returns:
+        - LLM response
+    """
+
+    # Retrieve prompt from template
+    prompt = retrieve_llm_prompt(prompt_type="prompt_context")
+
+    # Option to perform Retrieval Augmented Generation
+    if rag:
+        logger.info("\t Performing document retrieval")
+        context = doc_retrieval(pdf_content, prompt)
+        logger.debug("\nRetrieved context: \n %s", context)
+    else:
+        context = pdf_content
+
+    # Optional - Add context to prompt (full pdf_content or retrieved context)
+    if context:
+        prompt = prompt.format(context=context)
 
     messages = [
         {
@@ -67,12 +125,18 @@ def llm_extraction(
     return json_response
 
 
-def llm_qna(
-    text_model: str, mistral_client: object, pdf_file: str
-) -> Dict[str, Any]:
-    """Direct document Question & Answer - OCR + LLM combined"""
+def llm_qna(text_model: str, mistral_client: object, pdf_file: str) -> Dict[str, Any]:
+    """
+    Direct document Question & Answer - OCR + LLM combined
+    Args:
+        - text_model: LLM text model
+        - mistral_client: mistral client
+        - pdf_file: input PDF document
+    Returns:
+        - LLM response
+    """
 
-    prompt = generate_llm_prompt(prompt_type="prompt_doc", context="")
+    prompt = retrieve_llm_prompt(prompt_type="prompt_doc")
 
     # Getting the base64 string encoding
     base64_pdf = utils.encode_pdf(pdf_file)
@@ -81,16 +145,13 @@ def llm_qna(
         {
             "role": "user",
             "content": [
-                {
-                    "type": "text",
-                    "text": prompt
-                },
+                {"type": "text", "text": prompt},
                 {
                     "type": "document_url",
                     "document_url": f"data:application/pdf;base64,{base64_pdf}",
-                    "include_image_base64": True
-                }
-            ]
+                    "include_image_base64": True,
+                },
+            ],
         }
     ]
 
